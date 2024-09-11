588076f5718b948a03b31d4370bff39c068a9190
Charles Szymanski <charles37530@gmail.com>
2024-09-11 12:55:53 -0400

    camera updated Scene done

--- Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/CameraMovement.cs
+++ Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/CameraMovement.cs
@@ -7,13 +7,78 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] int sens;
     [SerializeField] int lockVertMin, lockVertMax;
     [SerializeField] bool invertY;
+  
+    public float normalFOV = 70f;
+    public float aimingFOV = 40f;
+    public float zoomSpeed = 60f;
+
+    public float leanAngle = 45f;
+    public float leanSpeed = 10f;
+    private float currentLeanAngle = 0f;
+
+    private bool isAiming;
+    private Camera cam;
 
     float rotX;
+
+    public int GetSens()
+    {
+        return sens;
+    }
+    public void SetSens(int value)
+    {
+        sens = value;
+    }
+    public int GetLockMin()
+    {
+        return lockVertMin;
+    }
+    public void SetVertMin(int value)
+    {
+        lockVertMin = Mathf.Clamp(value, -90, 90);
+    }
+    public int GetVertMax()
+    {
+        return lockVertMax;
+    }
+    public void SetVetMax(int value)
+    {
+        lockVertMax = Mathf.Clamp(value, -90, 90);
+    }
+    public float GetLeanAngle()
+    {
+        return leanAngle;
+    }
+    public void SetLeanAngel(float value)
+    {
+        leanAngle = Mathf.Clamp(value, 0, 90);
+    }
+    public float GetLeanSpeed()
+    {
+        return leanSpeed;
+    }
+    public void SetLeanSpeed(float value)
+    {
+        leanSpeed = Mathf.Max(value, 0);  
+    }
+    public bool GetInvertY()
+    {
+        return invertY;
+    }
+    public void SetInvertY(bool value)
+    {
+        invertY = value;
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        cam = GetComponent<Camera>();
+        cam.fieldOfView = normalFOV;
     }
 
     // Update is called once per frame
@@ -32,5 +97,39 @@ public class CameraMovement : MonoBehaviour
         transform.localRotation = Quaternion.Euler(rotX, 0, 0);
 
         transform.parent.Rotate(Vector3.up * mouseX);
+
+        HandelLeaning();
+
+        //AimLogic
+        if (Input.GetMouseButton(1))
+        {
+            isAiming = true;
+            cam.fieldOfView = Mathf.MoveTowards(cam.fieldOfView, aimingFOV, zoomSpeed * Time.deltaTime);
+
+        }
+        else
+        {
+            isAiming = false;
+            cam.fieldOfView = Mathf.MoveTowards(cam.fieldOfView, normalFOV, zoomSpeed * Time.deltaTime);
+
+        }
+    }
+
+    void HandelLeaning()
+    {
+        float targetLeanAngle = 0f;
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            targetLeanAngle = leanAngle;
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            targetLeanAngle = -leanAngle;
+        }
+
+        currentLeanAngle = Mathf.LerpAngle(currentLeanAngle, targetLeanAngle, Time.deltaTime * leanSpeed);
+
+        transform.localRotation = Quaternion.Euler(rotX, transform.localEulerAngles.y, currentLeanAngle);
     }
 }
