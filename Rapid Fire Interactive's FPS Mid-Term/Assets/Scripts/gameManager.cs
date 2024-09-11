440a96d63d182337f3be845ad83d5dd31d53fc0d
Ty-Seidel <fireblazeseidel55@gmail.com>
2024-09-11 19:38:04 -0400

    Trying to fix ui and tm pro (didn't work)

--- Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/gameManager.cs
+++ Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/gameManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 //using UnityEngine.UI;
-//using TMPro; // for using textmesh pro
+using TMPro; // for using textmesh pro
 
 public class gameManager : MonoBehaviour {
 
