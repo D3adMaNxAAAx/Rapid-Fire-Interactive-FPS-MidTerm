d9f78a11449a33798795a01ff4a84e67c2c31f0d
Carter Hardrick <159049215+D3adMaNxAAAx@users.noreply.github.com>
2024-09-11 23:24:12 -0400

    Set gamemanger ui controller

--- Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/playerMovement.cs
+++ Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/playerMovement.cs
@@ -158,7 +158,7 @@ public class playerMovement : MonoBehaviour, IDamage
     //Update UI HPBar
     public void updatePlayerUI() 
     {
-        //gameManager.instance.playerHPBar.fillAmount = (float)HP / HPOrig;
+        gameManager.instance.getHPBar().fillAmount = (float)HP / HPOrig;
     }
 
     public int getXP()
