2a4a6a4541dc394da8a030eb8386622167bcf150
Charles Szymanski <charles37530@gmail.com>
2024-09-23 13:45:40 -0400

    check point script done

---
+++ Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    [SerializeField] Renderer model;
+    Color colorOrig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && transform.position != gameManager.instance.getPlayerSpawnPos().transform.position)
+        {
+            gameManager.instance.setPlayerSpawnPos(this.gameObject); //setting spwan point
+            StartCoroutine(flashColor());
+        }
+    }
+    IEnumerator flashColor()
+    {
+        model.material.color = Color.blue;
+        gameManager.instance.getCheckPointPopup().SetActive(true); //Activate popup
+        yield return new WaitForSeconds(0.5f);
+        gameManager.instance.getCheckPointPopup().SetActive(false);  //Deactivate Popup
+        model.material.color = colorOrig;
+    }
+}
