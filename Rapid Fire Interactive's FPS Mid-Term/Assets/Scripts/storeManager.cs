eb05a902514e1ad817cc59717092e2734ba5fb21
Ben C <bryan.dejesus@student.allenisd.org>
2024-09-24 13:29:16 -0500

    Added temporary UI functionality to storeManager
    Cost, Restoration Display text, Player coins all now update. Still working on it

--- Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/storeManager.cs
+++ Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/storeManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class storeManager : MonoBehaviour
@@ -7,21 +8,34 @@ public class storeManager : MonoBehaviour
     // Singleton
     public static storeManager instance;
 
+    // Store Text
+    [SerializeField] TMP_Text healthText;
+    [SerializeField] TMP_Text ammoText;
+    [SerializeField] TMP_Text healthCostText;
+    [SerializeField] TMP_Text ammoCostText;
+    [SerializeField] TMP_Text playerCoinsText;
+    
     // Store Costs
+    [Header("-- Store Costs --")]
     [SerializeField] int healthCost;
     [SerializeField] int ammoCost;
 
     // Player Stats
     int playerHP;
     int playerAmmo;
+    int playerCoins;
 
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+        updateStoreUI();
     }
 
-    // Update() not needed yet.
+    void Update()
+    {
+        updateStoreUI(); // for now just update every frame
+    }
 
     // Public methods for external store functions -- these will call internal store functions as necessary
     // Buy Button Methods
@@ -50,9 +64,46 @@ public class storeManager : MonoBehaviour
         gameManager.instance.getPlayerScript().updatePlayerUI();
     }
 
+    void updateCoinsDisplay()
+    {
+        playerCoinsText.text = gameManager.instance.getPlayerScript().getCoins().ToString("F0");
+    }
+
+    void updateHealthDisplay()
+    {
+        // Update the Health Restoration Display
+        healthText.text = gameManager.instance.getPlayerScript().getHP().ToString("F0") + " > " + gameManager.instance.getPlayerScript().getHPOrig().ToString("F0");
+
+        // Update the Health Cost
+        healthCostText.text = "Cost: " + healthCost.ToString();
+
+        // Append Coin or Coins at the end
+        if (healthCost == 1) 
+            healthCostText.text += " coin";
+        else
+            healthCostText.text += " coins";
+    }
+
+    void updateAmmoDisplay()
+    {
+        // Update the Ammo Restoration Display
+        ammoText.text = gameManager.instance.getPlayerScript().getAmmo().ToString("F0") + " > " + gameManager.instance.getPlayerScript().getAmmoOrig().ToString("F0");
+
+        // Update the Ammo Cost
+        ammoCostText.text = "Cost: " + ammoCost.ToString();
+
+        // Append Coin or Coins at the end
+        if (ammoCost == 1)
+            ammoCostText.text += " coin";
+        else
+            ammoCostText.text += " coins";
+    }
+
     // Update the store UI as the player interacts with it
     void updateStoreUI()
     {
-
+        updateCoinsDisplay();
+        updateHealthDisplay();
+        updateAmmoDisplay();
     }
 }
