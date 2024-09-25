4edcd7dc527299625f8efd3c3de0475857cf9e36
Charles Szymanski <charles37530@gmail.com>
2024-09-25 19:02:32 -0400

    updated heal stats

--- Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/HealStats.cs
+++ Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/HealStats.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealStats : MonoBehaviour
+[CreateAssetMenu]
+public class HealStats : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string itemName;
+    public int healAmmount;
+    public AudioClip healSound;
+    public float healCoolDown;
 }
