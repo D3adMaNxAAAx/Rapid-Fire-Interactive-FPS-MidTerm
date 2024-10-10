d44ea24c26c51de686e591d3b6b5394b61dade78
Ty-Seidel <fireblazeseidel55@gmail.com>
2024-10-09 23:30:43 -0400

    Added base object pickup script

---
+++ Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/collectablePickup.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    [SerializeField] objectType type;
+    enum objectType { powerUp }
+
+    [SerializeField] AudioClip pickUPowerUpA;
+    float pickUPowerUpV = 1;
+
+    void Update() {
+        transform.Rotate(0, 0.25f, 0 * Time.deltaTime); // making pickup object spin
+    }
+
+    private void OnTriggerEnter(Collider otherObject) {
+        if (type == objectType.powerUp) {
+            if (otherObject.CompareTag("Player")) {
+                gameManager.manager.playerScript.getAudioSource().PlayOneShot(pickUpNutsA, pickUpNutsV); // playing audio once object is picked up
+                Destroy(gameObject);
+            }
+        }
+    }
+}
