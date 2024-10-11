6d70af2d572e03e793e8a06383577fdb1a86143d
Charles Szymanski <charles37530@gmail.com>
2024-10-11 17:08:18 -0400

    Update GunInventoryManger.cs

--- Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/GunInventoryManger.cs
+++ Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/GunInventoryManger.cs
@@ -1,23 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections.Generic;
 
 public class GunInventoryManager : MonoBehaviour
 {
-    [SerializeField] private List<Image> gunSlotImages; // List of UI images for gun slots
-    [SerializeField] private List<Text> gunAmmoTexts;   // List of UI texts for ammo counts
-    [SerializeField] private Color selectedGunColor;    // Color for the selected gun slot
-    [SerializeField] private Color defaultGunColor;     // Default color for gun slots
+    [SerializeField] private List<Sprite> gunSlotSprites; 
+    [SerializeField] private List<Text> gunAmmoTexts;
+    [SerializeField] private Color selectedGunColor;
+    [SerializeField] private Color defaultGunColor;
 
     private playerMovement player;
 
+
     void Start()
     {
-        // Initialize gun slots to empty and default color
-        for (int i = 0; i < gunSlotImages.Count; i++)
+        for (int i = 0; i < gunSlotSprites.Count; i++)
         {
-            gunSlotImages[i].color = defaultGunColor;
-            gunSlotImages[i].sprite = null;  // Empty sprite (no gun)          
+           
+            gunSlotSprites[i] = null;
             gunAmmoTexts[i].text = "";
         }
     }
@@ -27,30 +28,20 @@ public class GunInventoryManager : MonoBehaviour
         List<gunStats> guns = player.getGunList();
         int selectedGunIndex = player.getGunList().IndexOf(player.getCurGun());
 
-        for (int i = 0; i < gunSlotImages.Count; i++)
+        for (int i = 0; i < gunSlotSprites.Count; i++)
         {
             if (i < guns.Count)
             {
-                // Set the sprite from the gunStats
-                gunSlotImages[i].sprite = guns[i].gunSprite;
+                
+                gunSlotSprites[i] = guns[i].gunIcon; 
 
-                // Update ammo text
+                // Update the ammo count text
                 gunAmmoTexts[i].text = guns[i].ammoCur.ToString() + " / " + guns[i].ammoMag.ToString();
-
-                // Highlight the selected gun
-                if (i == selectedGunIndex)
-                {
-                    gunSlotImages[i].color = selectedGunColor;
-                }
-                else
-                {
-                    gunSlotImages[i].color = defaultGunColor;
-                }
             }
             else
             {
-                // Clear empty slots
-                gunSlotImages[i].sprite = null;
+                // Set to default empty sprite if no gun is present
+                gunSlotSprites[i] = null;
                 gunAmmoTexts[i].text = "";
             }
         }
