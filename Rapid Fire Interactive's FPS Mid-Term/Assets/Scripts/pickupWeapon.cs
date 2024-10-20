d9a7db000e8a8a18252cf6d0558b93dcac11e5fa
Bryan De Jesus <bryandejpers@gmail.com>
2024-10-20 15:20:37 -0500

    Gun Pickup Interact
    Guns pickups will now need interact input from the player

--- Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/pickupWeapon.cs
+++ Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/pickupWeapon.cs
@@ -6,12 +6,31 @@ public class pickupWeapon : MonoBehaviour
 {
     [SerializeField] gunStats gun;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            gameManager.instance.getPlayerScript().getGunStats(gun);
-            Destroy(gameObject);
+            // Show interact menu
+            if (!gameManager.instance.getInteractUI().activeInHierarchy)
+                gameManager.instance.getInteractUI().SetActive(true);
+
+            if (Input.GetButton("Interact"))
+            {
+                interact();
+            }
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Hide interact menu
+        if (gameManager.instance.getInteractUI().activeInHierarchy)
+            gameManager.instance.getInteractUI().SetActive(false);
+    }
+
+    public void interact()
+    {
+        gameManager.instance.getPlayerScript().getGunStats(gun);
+        Destroy(gameObject);
     }
 }
