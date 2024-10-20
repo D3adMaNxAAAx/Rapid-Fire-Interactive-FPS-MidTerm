8cbd1f179e059e937b838e926a346b2057ced787
Carter Hardrick <159049215+D3adMaNxAAAx@users.noreply.github.com>
2024-10-20 12:13:08 -0400

    Objective sysy still working on moving to different card for time being

--- Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/objectiveSystem.cs
+++ Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/objectiveSystem.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class objectiveSystem : MonoBehaviour
 {
     public static objectiveSystem instance;
 
-    [SerializeField] List<objectives> objectivesList;
+   
 
     [SerializeField] objectives obj1;
     [SerializeField] objectives obj2;
@@ -25,15 +27,7 @@ public class objectiveSystem : MonoBehaviour
     void Awake()
     {
         instance = this;
-        objectivesList.Add(obj1);
-        objectivesList.Add(obj2);
-        objectivesList.Add(obj3);
-        objectivesList.Add(obj4);
-        objectivesList.Add(obj5);
-        objectivesList.Add(obj6);
-        objectivesList.Add(obj7);
-        objectivesList.Add(obj8);
-        objectivesList.Add(obj9);
+       
     }
 
     // Update is called once per frame
@@ -44,9 +38,14 @@ public class objectiveSystem : MonoBehaviour
 
     void objectiveComplete()
     {
-        if (objectivesList != null)
+        if (obj1 != null)
         {
-
+            if (playerStats.Stats.getPowerObjects() >= 3 && playerStats.Stats.getPowerObjects() < 6)
+            { 
+                obj1.isCompleted = true;
+                
+            
+            }
         }
     }
 
