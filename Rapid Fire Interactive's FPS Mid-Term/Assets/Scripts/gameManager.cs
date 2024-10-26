bec57596bf4aff43e1025bbfd4f108929e1c656d
Charles Szymanski <charles37530@gmail.com>
2024-10-26 19:10:19 -0400

    music

--- Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/gameManager.cs
+++ Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/gameManager.cs
@@ -308,20 +308,23 @@ public class gameManager : MonoBehaviour {
         yield return new WaitForSeconds(audioManager.instance.VictoryA.length);
 
         // After the short clip, play the longer victory music
-        audioSource.clip = audioManager.instance.VictoryMusicA;
+        audioSource.clip = audioManager.instance.VictoryA;
         audioSource.volume = audioManager.instance.VictoryMusicVol; // This should use the configured volume
         audioSource.loop = false;
         audioSource.Play();
     }
 
     void youWin() {
+        AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.outputAudioMixerGroup = audioManager.instance.MusicMixerGroup;
         if (menuActive != null) {
             menuActive.SetActive(false);
         }
         statePause();
         menuActive = menuWin; // Set Win menu as active
         menuActive.SetActive(true); // Show Win menu
-        EventSystem.current.SetSelectedGameObject(winMenuFirst);  // Set eventsystem selected game object to the button assigned
+        EventSystem.current.SetSelectedGameObject(winMenuFirst);
+        audioSource.PlayOneShot(audioManager.instance.VictoryMusicA, audioManager.instance.VictoryVol);// Set eventsystem selected game object to the button assigned
     }
     public void openWinStatsMenu()
     {
