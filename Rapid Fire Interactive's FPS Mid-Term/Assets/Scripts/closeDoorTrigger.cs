ae05c558af7a70a1915d00e01ea695d4d76f5678
Bryan De Jesus <bryandejpers@gmail.com>
2024-10-27 15:15:15 -0500

    Safe Room doors are now fully fixed
    Exit doors close properly now

--- Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/closeDoorTrigger.cs
+++ Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/closeDoorTrigger.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class closeDoorTrigger : MonoBehaviour
 {
     [SerializeField] safeRoom safe;
+    [SerializeField] doorControllerManual exit;
 
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             // Check which door to close
-            if (doorControllerManual.instance != null)
+            if (exit != null)
             {
-                if (doorControllerManual.instance.getDoorStatus())
-                    StartCoroutine(doorControllerManual.instance.closeDoor());
+                if (exit.getDoorStatus())
+                    StartCoroutine(exit.closeDoor());
             }
 
             if (safe != null)
