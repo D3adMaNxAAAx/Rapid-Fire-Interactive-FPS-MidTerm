d68102e57a697c26387f505408c711a2e5ff8cfc
Ty-Seidel <fireblazeseidel55@gmail.com>
2024-12-26 16:14:07 -0500

    Added object pack, commented out peek code

--- Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/CameraMovement.cs
+++ Rapid Fire Interactive's FPS Mid-Term/Assets/Scripts/CameraMovement.cs
@@ -90,7 +90,7 @@ public class CameraMovement : MonoBehaviour
 
         transform.parent.Rotate(Vector3.up * mouseX);
 
-        HandleLeaning();
+        // HandleLeaning(); // peeking
 
         //AimLogic
         if (Input.GetMouseButton(1)) {
